668368c416c712e30cbe983198f80009d0ae42be
Andrii-Semeniv <andrii.semeniv7@gmail.com>
2023-01-30 00:12:09 +0200

    firstTaskDone

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+function createGalleryItems(items) {
+  return items
+    .map(({ original, preview, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src= "${preview}";
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+    `;
+    })
+    .join("");
+}
+const items = createGalleryItems(galleryItems);
+gallery.innerHTML = items;
+
+gallery.addEventListener("click", onHandleClick);
+
+function onHandleClick(evt) {
+  evt.preventDefault();
+
+  basicLightbox
+    .create(
+      `
+     <img src = ${evt.target.dataset.source}
+	> `
+    )
+    .show();
+}
